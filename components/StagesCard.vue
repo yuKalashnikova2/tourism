67f53b9f35647605ec3ce998e63c5f65d9f21540
yuKalasnikova2 <pmalian@list.ru>
2024-10-20 16:48:36 +0300

    refsctor styles card page

--- components/StagesCard.vue
+++ components/StagesCard.vue
@@ -21,15 +21,17 @@ const styleBackground = computed(() => {
 <template>
     <div class="stages-card">
         <div class="stages-card__title" :style="{ backgroundColor: color }">
-            <h3>{{ title }}<br>{{ title2 }}</h3>
+            <h3>{{ title }}<br />{{ title2 }}</h3>
         </div>
 
-        <div class="stages-card__desc__border" :style="{ backgroundColor: color }">
+        <div
+            class="stages-card__desc__border"
+            :style="{ backgroundColor: color }"
+        >
             <div class="stages-card__desc" :class="styleBackground">
-            <div>{{ desc }}</div>
+                <div>{{ desc }}</div>
+            </div>
         </div>
-        </div>
-        
     </div>
 </template>
 
@@ -43,6 +45,11 @@ const styleBackground = computed(() => {
     border-radius: 60px;
     box-shadow: 2px 8px 16px 0px rgba(188, 188, 188, 1);
 
+    //    &__border-morder {
+    //     background-color: red;
+    //     position: relative;
+    //     z-index: 10;
+    //    }
     &__title {
         color: $white;
         padding: 20px;
@@ -50,6 +57,21 @@ const styleBackground = computed(() => {
         border-top-left-radius: 60px;
         width: 100%;
         text-align: center;
+        &::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 1;
+            border-radius: 60px;
+            background: rgba(199, 202, 243, 0.1);
+
+            pointer-events: none;
+        }
         & h3 {
             font-weight: 700;
             font-size: 40px;
@@ -78,17 +100,20 @@ const styleBackground = computed(() => {
         padding-right: 20px;
         margin-bottom: 20px;
         padding-left: 40px;
-        background: linear-gradient(90deg, #FFFFFF 0%, rgba(255, 255, 255, 0.8) 100%);
+        background: linear-gradient(
+            90deg,
+            #ffffff 0%,
+            rgba(255, 255, 255, 0.8) 100%
+        );
         min-height: auto;
         &__border {
             border-bottom-left-radius: 60px;
             border-bottom-right-radius: 60px;
 
-    display: flex;
-    height: 100%;
+            display: flex;
+            height: 100%;
         }
 
-        
         // z-index: 1;
         @media (max-width: 768px) {
             font-size: 20px;
